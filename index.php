abc22cd6f4dcdd4b9c122fcc9bb1d92fcbc84c2c
Yogesh5454573 <bachutey@gmail.com>
2025-12-27 12:00:31 +0530

    Refactor index.php: remove unused button elements and correct script paths

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,10 +10,8 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <body>
     <?php include('commen/header.php'); ?>
-
     <div class="wrapper">
         <div class="container">
             <div class="row">
@@ -22,30 +19,17 @@
                 <div class="span9">
                     <div class="content">
                         <div class="btn-controls">
-                            <div class="btn-box-row row-fluid">
-                                <a href="#" class="btn-box big span4"><i class=" icon-random"></i><b>65%</b>
-                                    <p class="text-muted">
-                                        Growth</p>
-                                </a><a href="#" class="btn-box big span4"><i class="icon-user"></i><b>15</b>
-                                    <p class="text-muted">
-                                        New Users</p>
-                                </a><a href="#" class="btn-box big span4"><i class="icon-money"></i><b>15,152</b>
-                                    <p class="text-muted">
-                                        Profit</p>
-                                </a>
-                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
     <?php include 'commen/footer.php'; ?>
-    <script src="scripts/jquery-1.9.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="scripts/datatables/jquery.dataTables.js"></script>
-    <script src="scripts/common.js"></script>
+    <script src="asset/scripts/jquery-1.9.1.min.js"></script>
+    <script src="asset/bootstrap/js/bootstrap.min.js"></script>
+    <script src="asset/scripts/datatables/jquery.dataTables.js"></script>
+    <script src="asset/scripts/common.js"></script>
 </body>
 
 </html>
